d6be73fbc579c21350ead90c10891c5d5f90cee1
Jason Guardame <emman.guardame@gmail.com>
2022-09-15 16:18:46 +0800

    created gfourapp for our frontend needs

---
+++ gfourapp/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+
+function App() {
+
+  // Add Webpages here once miss jass finished their side
+  return (
+    <div className="App">
+      <BrowserRouter>
+          
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
