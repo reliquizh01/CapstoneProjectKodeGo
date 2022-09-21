bdfab09d52a6268c98ebb9698cfa909e092cbd0a
Jason Guardame <emman.guardame@gmail.com>
2022-09-21 11:00:43 +0800

    removed server on front-end repository

--- Server/Routes/users.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../Controller/userController');
-
-router.post('/register', userController.register);
-
-module.exports = router;
